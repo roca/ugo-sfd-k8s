7d12eb8bf681f4a0b65c8393a6d729ff8a2a54a4
Romel Campbell <RomelCampbell@gmail.com>
2024-09-10 03:54:04 -0400

    WIP; Setting up api mux handler

--- apis/services/sales/mux/mux.go
+++ apis/services/sales/mux/mux.go
@@ -2,10 +2,25 @@
 // to the application mux.
 package mux
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 // WebAPI constructs a http.Handler with all application routes bound.
-func WebAPI() http.Handler {
+func WebAPI() *http.ServeMux {
+	mux := http.NewServeMux()
 
-	return nil
+	h := func(w http.ResponseWriter, r *http.Request) {
+		status := struct {
+			Status string
+		}{
+			Status: "OK",
+		}
+
+		json.NewEncoder(w).Encode(status)
+
+	mux.HandleFunc("GET /test", h)
+
+	return mux
 }
