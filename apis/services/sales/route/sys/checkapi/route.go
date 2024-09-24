588bf8a707318183c693af7b3995e70a3f6ee341
romel.campbell <romel.campbell@regeneron.com>
2024-09-24 15:52:00 -0400

    21: No metrics around liveliness/readynees

--- apis/services/sales/route/sys/checkapi/route.go
+++ apis/services/sales/route/sys/checkapi/route.go
@@ -3,8 +3,8 @@ package checkapi
 import "github.com/roca/ugo-sfd-k8s/foundation/web"
 
 func Routes(app *web.App) {
-	app.HandleFunc("GET /liveliness", liveliness)
-	app.HandleFunc("GET /readiness", readiness)
+	app.HandleFuncNoMiddleware("GET /liveliness", liveliness)
+	app.HandleFuncNoMiddleware("GET /readiness", readiness)
 	app.HandleFunc("GET /testerror", testError)
 	app.HandleFunc("GET /testpanic", testPanic)
 }
