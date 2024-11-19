f826a68781a5e1977d6e164f745984cf0dcfac9d
romel.campbell <romel.campbell@regeneron.com>
2024-11-19 11:13:44 -0500

    WIP: Gettingh token

--- client/main.go
+++ client/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,9 +12,40 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	fmt.Println("Serving http://localhost:8080/world")
+	// token, err := getToken()
+	// if err != nil {
+	// 	log.Println(err)
+	// }
+
+	// fmt.Printf("Auth token: %s\n", token)
 	err := http.ListenAndServe(":8080", http.HandlerFunc(handler))
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
+}
+
+func basicAuth(username, password string) string {
+	auth := username + ":" + password
+	return base64.StdEncoding.EncodeToString([]byte(auth))
+}
+
+func getToken() (string, error) {
+
+	req, err := http.NewRequest("GET", "http://auth-service:8080/token/54bb2165-71e1-41a6-af3e-7da4a0e1e2c1", nil)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Add("Authorization", "Basic "+basicAuth("admin@example.com", "gophers"))
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+
+	var tokenBytes []byte
+	_, err = resp.Body.Read(tokenBytes)
+	if err != nil {
+		return "", err
+	}
+
+	return string(tokenBytes), nil
 }
